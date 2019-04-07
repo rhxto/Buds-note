2aedaf13d19b86d708338db6ef71de10318a2e05
Fede <fedebosi04@gmail.com>
2019-04-07 22:20:59 +0200

    Aggiunta altri files e modifiche a write.php

---
+++ form/js.js
@@ -0,0 +1,25 @@
+var errThrown = false;
+function submitform() {
+  document.forms["form"].submit();
+}
+function hasWhiteSpace(s) {
+  return s.indexOf(' ') >= 0;
+}
+function testInput() {
+  var usr = $("#Username").val();
+  var pswd = $("#Password").val();
+
+  if(usr == null || pswd == null || usr == undefined || pswd == undefined || usr == "" || pswd == "") {
+    if (errThrown == false) {
+      $("body").append("Inserire username e password validi");
+      errThrown = true;
+    }
+  } else if (hasWhiteSpace(usr) == false && hasWhiteSpace(pswd) == false) {
+    submitform();
+  } else {
+    if (errThrown == false) {
+      $("body").append("Inserire username e password validi");
+      errThrown = true;
+    }
+  }
+}
---
+++ mysql/write.php
@@ -0,0 +1,53 @@
+<?php
+  function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    if ($data == "") {
+      error_log("Username or password invalid after script chars trim, ignoring.");
+      echo "Inserire username e password validi.";
+      die();
+    }
+    return $data;
+  }
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["Username"]) || $_POST["Username"] == "") {
+      error_log("POST username value null, ignoring.");
+      echo "Inserire username e password validi.";
+      die();
+    } else {
+      $Username = test_input($_POST["Username"]);
+    }
+
+    if (empty($_POST["Password"]) || $_POST["Password"] == "") {
+      error_log("POST password value null, ignoring.");
+      echo "Inserire username e password validi.";
+      die();
+    } else {
+      $Password = test_input($_POST["Password"]);
+    }
+  }
+
+  $UsernameDb = '"' . $Username . '"';
+  $PasswordDb = '"' . $Password . '"';
+
+  $mysqlServer = "localhost";
+  $mysqlUser = "system";
+  $mysqlPasswd = "the_best_admin_passwd";
+
+  try {
+    $conn = new PDO("mysql:host=$mysqlServer;dbname=Logins", $mysqlUser, $mysqlPasswd);
+    echo "Connected successfully to mysql!";
+    $conn->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Excepion errmode set!";
+    $conn->exec("USE Logins;");
+    echo "Database selected successfully!";
+    $conn->exec("INSERT INTO Logins (Username, Password) VALUES ($UsernameDb, $PasswordDb)");
+    echo "Done!";
+    $conn = null;
+  } catch(PDOException $e) {
+    echo "Connection failure: " . $e->getMessage();
+    $conn = null;
+  }
+?>
