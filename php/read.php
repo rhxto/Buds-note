793ad96988ed03b54f59210429d857e6b4542a0f
Fede <fedebosi04@gmail.com>
2019-04-11 15:16:53 +0200

    Rimozione leg di non validitá della mail nel log di apache perché inutile.

--- php/read.php
+++ php/read.php
@@ -5,7 +5,6 @@
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
     if ($data == "") {
-      error_log("Data invalid after script chars trim, ignoring.");
       die("Invalid username, password or email");
     }
     return $data;
@@ -13,14 +12,12 @@
 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["Username"]) || $_POST["Username"] == "") {
-      error_log("POST username value null, ignoring.");
       die("Invalid username or password");
     } else {
       $Username = test_input($_POST["Username"]);
     }
 
     if (empty($_POST["Password"]) || $_POST["Password"] == "") {
-      error_log("POST password value null, ignoring.");
       die("Invalid username or password");
     } else {
       $Password = test_input($_POST["Password"]);
