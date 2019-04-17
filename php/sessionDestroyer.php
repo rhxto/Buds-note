3c9b00b297a60433c6f1a405420145b12a3f54fd
root <root@localhost.localdomain>
2019-04-17 21:13:52 +0200

    Cosa di prima su php side.

--- php/sessionDestroyer.php
+++ php/sessionDestroyer.php
@@ -2,11 +2,14 @@
 if (isset($_POST['action']) && $_POST['action'] == "logout") {
   if ($_SESSION['logged_in'] = '1') {
     $_SESSION['logged_in'] = '0';
+    return true;
   } else {
     $ipL = $_SERVER['REMOTE_ADDR'];
     error_log("**LOGOUT ESEGUITO SENZA ESSERSI LOGGATO** IP:$ipL");
+    return false;
   }
 } else {
   error_log("**LOGOUT ESEGUITO SENZA ESSERSI LOGGATO** IP:$ipL");
+  return false;
 }
 ?>
