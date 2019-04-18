45131a490328026d9bde9a327f0a2774cad3b261
Fede <fedebosi04@gmail.com>
2019-04-18 12:45:35 +0200

    Rimozione files inutili

--- node-test/pagina.php
+++
@@ -1,9 +0,0 @@
-<?php
-  error_log(print_r($_POST, true));
-  /*
-  if(isset($_POST["data"])) {
-    $file = fopen("dati.txt", "a+") or die("errore");
-    fwrite($file, $_POST["data"]);
-    fclose($file);
-  }*/
-?>
