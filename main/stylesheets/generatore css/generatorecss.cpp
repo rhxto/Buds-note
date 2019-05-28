9901d0ba9b1ddbafddfc699fba01e0cff5ea9c7e
Edoebzd <38160197+Edoebzd@users.noreply.github.com>
2019-05-28 16:21:10 +0200

    cambio range da 0/100 a 0/200

--- main/stylesheets/generatore css/generatorecss.cpp
+++ main/stylesheets/generatore css/generatorecss.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 int main(){
-    freopen("output.txt","w",stdout);
-    
-	for(int i=0;i<=100;i++){
+    freopen("positions.css","w",stdout);
+
+	for(int i=0;i<=200;i++){
 		cout<<".top"<<i<<" {top: "<<i<<"%;}"<<endl;
 	}
-	for(int i=0;i<=100;i++){
+	for(int i=0;i<=200;i++){
 		cout<<".bottom"<<i<<" {bottom: "<<i<<"%;}"<<endl;
 	}
-	for(int i=0;i<=100;i++){
+	for(int i=0;i<=200;i++){
 		cout<<".right"<<i<<" {right: "<<i<<"%;}"<<endl;
 	}
-	for(int i=0;i<=100;i++){
+	for(int i=0;i<=200;i++){
 		cout<<".left"<<i<<" {left: "<<i<<"%;}"<<endl;
 	}
-	for(int i=0;i<=100;i++){
+	for(int i=0;i<=200;i++){
 		cout<<".height"<<i<<" {height: "<<i<<"%;}"<<endl;
 	}
-	for(int i=0;i<=100;i++){
+	for(int i=0;i<=200;i++){
 		cout<<".width"<<i<<" {width: "<<i<<"%;}"<<endl;
 	}
 }
