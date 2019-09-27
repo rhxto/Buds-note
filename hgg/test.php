12ceeb659c81fbedef532617522278184cb2cca4
hgg022 <hggthegamer@gmail.com>
2019-09-27 12:46:31 +0200

    aggiunta funzione

--- hgg/test.php
+++ hgg/test.php
@@ -6,5 +6,9 @@
     -moltiplicare 2 numeri
     -stampare la "concatenazione di due stringhe"
   */
-  echo  "questo é un test";
+  function Sommadi2numeri(int $fattore1, int $fattore2) {
+  return $fattore1 + $fattore2;
+}
+$somma = Sommadi2numeri(11, 34)
+echo "<br/> $somma"
  ?>
