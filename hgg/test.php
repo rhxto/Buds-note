03a57c545075a6bb6b7e127f5f6e434834e2efb7
hgg022 <hggthegamer@gmail.com>
2019-09-27 13:05:06 +0200

    Aggiunte altre funzioni

--- hgg/test.php
+++ hgg/test.php
@@ -10,5 +10,23 @@
   return $fattore1 + $fattore2;
 }
 $somma = Sommadi2numeri(11, 34)
-echo "<br/> $somma"
+echo "<br/> $somma";
+
+function Moltiplicazionedi2numeri(int $fattore1a, int $fattore2a) {
+return $fattore1a + $fattore2a;
+}
+$prodotto = Moltiplicazionedi2numeri(29, 12)
+echo "<br/> $prodotto";
+
+function Sottrazionedi2numeri(int $fattore1b, int $fattore2b) {
+return $fattore1b + $fattore2b;
+}
+$differenza = Sottrazionedi2numeri(43, 18)
+echo "<br/> $differenza";
+
+function Divisionedi2numeri(int $fattore1c, int $fattore2c) {
+return $fattore1c + $fattore2c;
+}
+$quoziente = Divisionedi2numeri(11, 34)
+echo "<br/> $quoziente";
  ?>
