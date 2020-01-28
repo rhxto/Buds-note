b96d8181fe90094d79647c2821e50b0af26e1254
Fedefreez <fedebosi04@gmail.com>
2020-01-28 15:15:03 +0100

    Aggiunta ban

--- register/index.php
+++ register/index.php
@@ -4,6 +4,9 @@
   header("Cache-Control: no-store, no-cache, must-revalidate");
   header("Cache-Control: post-check=0, pre-check=0", false);
   header("Pragma: no-cache");
+  require '../php/ips.php';
+  $ip = $_SERVER['REMOTE_ADDR'];
+  loginCheck($ip);
 ?>
 <!DOCTYPE html>
 <html>
