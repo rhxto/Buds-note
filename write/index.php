ed12231df7fe4763f680add468691878cb5aeece
Leodom01 <leonardodominici01@gmail.com>
2020-01-29 16:58:53 +0100

    Write checkbox e register minor Ho sistemato in modo carino la scelta dell'anno su write (si può scegliere solo un anno) fatto in modo che sia uguale a quello di serach, bisogna iniziare a fare le cose uniformi e su registeer ho cambiato solo il nome della scheda

--- write/index.php
+++ write/index.php
@@ -1,40 +1,64 @@
-<?php
-  require '../php/ips.php';
-  $ip = $_SERVER['REMOTE_ADDR'];
-  loginCheck($ip);
- ?>
+<?php session_start();
+  header("Expires: 0");
+  header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+  header("Cache-Control: no-store, no-cache, must-revalidate");
+  header("Cache-Control: post-check=0, pre-check=0", false);
+  header("Pragma: no-cache");
+?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="UTF-8">
-  <title>Bud's note login</title>
+  <title>Bud's note search</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="../jquery/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="../jQuery.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
   <script src="../bootstrap/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="writeNote.css" />
-  <script src="../main/errorHandler.js"></script>
-  <script src="writeNote.js"></script>
+  <script src="search.js"></script>
+  <link rel="stylesheet" href="search.css" type="text/css" />
 </head>
 <body>
-  <div class="localWarn alert alert-danger alert-dismissible">
-    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-    <strong>Warning! </strong><span id="localWarn"></span>
-  </div>
-  <div id="animation">
-  </div>
-  <div class="pageContainer">
-    <div class="wrapper fadeInDown">
-      <div id="formContent">
-      </br>
-        <h4>Upload your note!</h4>
-        <br/>
-        <input type="text" id="Title" class="fadeIn second" placeholder="Title:"/>
-        <br/>
+<div class="wrapper fadeInDown">
+  <div id="formContent">
+    <div class="fadeIn first">
+      <br/>
+      <h4>Search note:</h4>
+    </div>
+    <input type="text" id="Title" class="fadeIn first" name="title" placeholder="Title">
+    <p/>
+    <div class="dropdown">
+      <button type="button" class="btn btn-primary dropdown-toggle fadeIn second" onclick="selectSubj(this);" id="Subject" data-toggle="dropdown">
+        Subject
+      </button>
+      <div class="dropdown-menu dropdown-menu-right">
+        <?php
+          require_once "../php/core.php";
+          require_once '../php/funs.php';
+          $response = subj(connectDb(), NULL, NULL);
+          foreach ($response[1] as $subj) {
+            echo "<a class='dropdown-item' onclick='selectSubj(this)'>" . $subj . "</a>";
+          }
+        ?>
+      </div>
+    </div><p/>
+    <div class="dropdown">
+      <button type="button" class="btn btn-primary dropdown-toggle fadeIn second" onclick="selectDept(this);" id="Dept" data-toggle="dropdown">
+        Department
+      </button>
+      <div class="dropdown-menu dropdown-menu-right">
+        <?php
+          $response = dept(connectDb(), NULL, NULL);
+          foreach ($response[1] as $dept) {
+            echo "<a class='dropdown-item' onclick='selectDept(this)'>" . $dept . "</a>";
+          }
+        ?>
+      </div>
+    </div><p/>
+      <p/>
       <div class="container">
-        <p>Year:</p>
-        <div class="btn-group btn-group-toggle" data-toggle="buttons">
+        <p class='fadeIn third'>Year:</p>
+        <div class="btn-group btn-group-toggle fadeIn third" data-toggle="buttons">
           <label class="btn btn-info">
             <input type="radio" name="options" id="year_1" autocomplete="off"> 1
           </label>
@@ -53,48 +77,11 @@
         </div>
       </br>
       </div>
-      <div class="dropdown">
-        <br/>
-          <button type="button" class="btn btn-primary dropdown-toggle" id="Subject" data-toggle="dropdown">
-            Subject
-        </button>
-        <div class="dropdown-menu dropdown-menu-right">
-          <?php
-            require_once "../php/core.php";
-            require_once '../php/funs.php';
-            $response = subj(connectDb(), NULL, NULL);
-            foreach ($response[1] as $subj) {
-              echo "<a class='dropdown-item' onclick='selectSubj(this)'>" . $subj . "</a>";
-            }
-          ?>
-        </div>
-      </div>
-      <div class="dropdown">
-        <br/>
-        <button type="button" class="btn btn-primary dropdown-toggle" id="Dept" data-toggle="dropdown">
-          Department
-        </button>
-        <div class="dropdown-menu dropdown-menu-right">
-          <?php
-            $response = dept(connectDb(), NULL, NULL);
-            foreach ($response[1] as $dept) {
-              echo "<a class='dropdown-item' onclick='selectDept(this)'>" . $dept . "</a>";
-            }
-          ?>
-        </div>
-      </div>
-      <br/>
-      <p/>
-        <textarea class="form-control" rows="25" id="Content" placeholder="Write here your note..."></textarea>
-        <div id="file-upload" class="container">
-          <div class="custom-file">
-            <input type="file" class="custom-file-input" id="uploadImage">
-            <label class="custom-file-label" for="uploadImage">Upload here your images...</label>
-          </div>
-        </div>
-        <input onclick="submitNote();" type="submit" class="fadeIn fifth" value="Upload">
-      </div>
-    </div>
+    <br/>
+    <input type="text" id="User" class="fadeIn fourth" name="login" placeholder="User">
+    <input type="submit" class="fadeIn fourth" value="Search" onclick="testInput()">
   </div>
+</div>
+
 </body>
 </html>
