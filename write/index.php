dbe5b449d2361e2477dfeb58356842014b66d0b1
Leodom01 <leonardodominici01@gmail.com>
2020-01-30 06:16:17 +0100

    Minor fix
    Sistemato un path

--- write/index.php
+++ write/index.php
@@ -12,7 +12,7 @@
   <title>Bud's note search</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-  <script src="../jQuery.js"></script>
+  <script src="../jquery/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
   <script src="../bootstrap/js/bootstrap.min.js"></script>
   <script src="search.js"></script>
