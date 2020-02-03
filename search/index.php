3d6e1c1389b85fc5172e2eb3595b500e2fae5904
Leodom01 <leonardodominici01@gmail.com>
2020-02-03 06:48:03 +0100

    Impostazione iniziale noteFeed Ho sitemato la part e di note feed dove andranno a finire tutte le note che rientrano nella selezione degli utenti, quello a riga 87/88/89 è un prototipo (non ancora finito) di come dovrà essere fatta una scheda di una delle note in output

--- search/index.php
+++ search/index.php
@@ -80,6 +80,16 @@
     <input type="text" id="User" class="fadeIn third" name="login" placeholder="User">
     <input type="submit" class="fadeIn fourth" value="Search" onclick="testInput()">
   </div>
+  <h2>Your note feed:</h2>
+  <div id="noteFeed" style="visibility : visible">
+    <!-- Da qui fino alla fine del div metteremo la parte per le risposte con le anteprime delle note -->
+    <!-- Ora creo un esempio di come dovrà essere una nota -->
+    <div id="formContent fadeIn first">
+      <a id="noteName" href="Note page link" class="text-decoration-none text-left fadeIn second">Here note name</a>
+      <a id="noteOwner" href="User page link" class="text-decoration-none text-right fadeIn second">User name here</a>
+    </div>
+
+  </div>
 </div>
 
 </body>
